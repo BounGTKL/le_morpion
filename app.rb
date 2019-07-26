4c038a7b6ae601b4c6110a190db2dc55e7a2f506
BounGTKL <39437629+BounGTKL@users.noreply.github.com>
2019-07-26 08:14:10 +0200

    Add files via upload

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require "./lib/GameBoard.rb"
 require "./lib/Player.rb"
 
 def switch_player
-	@current_player == @player_1.name ? @current_player = @player_2.name : @current_player = @player_1.name
+	@current_player == @player_1.name ? @current_player = @player_2.name : @current_player = @player_1.name    					#We define the switching between the two players
 end
 
 @player_1 = Player.new
@@ -10,11 +10,11 @@ end
 @current_player = @player_1.name
 
 puts
-puts "#{@player_1.name} and #{@player_2.name} are ready to play Le morpion!"
+puts "#{@player_1.name} and #{@player_2.name} are ready to play Le morpion!"																					#The intro after the two players gives there name
 puts
 gameboard = GameBoard.new
 
-until gameboard.win || gameboard.draw do
+until gameboard.win || gameboard.draw do 																																						  #We inform about the game and show them differents moves 
 	puts "These are the available moves"
 	puts gameboard.list_available_moves
 	puts "#{@current_player}, where would you like to move?"
